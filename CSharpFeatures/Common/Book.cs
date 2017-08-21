166408f77dc66a90720d5a70d11053b9520effe5
Franco Arratia <franco.arratia@dharbor.com>
2017-08-21 01:08:00 -0400

    Getting started reviewing new C# features.

---
+++ CSharpFeatures/Common/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common
+{
+    public class Book
+    {
+        public String Isbn { get; set; }
+
+        public String Title { get; set; }
+
+    }
+}
---
+++ CSharpFeatures/Generics/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class Utilities
+    {
+        public T Max<T>(T a, T b) where T : IComparable
+        {
+            return a.CompareTo(b)>0 ? a : b;
+        }
+    }
+}
