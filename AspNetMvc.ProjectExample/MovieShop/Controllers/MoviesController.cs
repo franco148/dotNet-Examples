2f721c3d3143c3fe9a992e7261b45d652c4312ac
Franco Arratia <franco.arratia@dharbor.com>
2017-08-30 23:33:00 -0400

    Getting started with ASP.NET MVC 5

---
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShop.Models;
+
+namespace MovieShop.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Ramdom
+        public ActionResult Ramdom()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            //We can send the object to be rendered.
+            return View(movie);
+        }
+    }
+}
