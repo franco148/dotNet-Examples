da7d0e52c77457c140b884fc9e680c9a4c91c48d
DotNetCoder <robert_al_16_2@hotmail.com>
2017-08-30 20:03:08 -0400

    Getting started with HangFire

---
+++ HangFire.Examples/Highlighter/Models/HighlighterDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Highlighter.Models
+{
+    public class HighlighterDbContext : ApplicationDbContext
+    {
+    }
+}
---
+++ HangFire.Examples/Highlighter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Highlighter.Startup))]
+namespace Highlighter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
