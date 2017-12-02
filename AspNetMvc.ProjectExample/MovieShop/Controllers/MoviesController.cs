a969049d42fd7561d1c18212c6beac1613427b5f
beto <franck_ral@yahoo.com>
2017-12-01 21:51:52 -0400

    Fixed version compatibility of .Net Framework

---
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShop.Models;
+
+namespace MovieShop.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+            return View(movie);
+            //return Content("Hello world!!!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "n" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + " / " + month);
+        }
+    }
+}
---
+++ AspNetMvc.ProjectExample/README.md
@@ -0,0 +1,20 @@
+# ASP NET MVC Concepts
+
+ActionResult: This is like a general object of results. The specific ones would be.
+
+	TYPE						HELPER METHOD
+- ViewResult 				- 	View()
+- PartialViewResult			-	PartialView()
+- ContentResult				- 	Content()					: Simple text
+- RedirectResult			-	Redirect()
+- RedirectToRouteResult		-	RedirectToAction()
+- JsonResult				-	Json()
+- FileResult				-	File()
+- HttpNotFoundResult		-	HttpNotFound()
+- EmptyResult				- 	When action is not going to return any value, like VOID.
+
+
+
+# CODE SNIPETS
+- prop tab
+- mvcaction4 tab
