11d2a561bb0d8107469b02512072c9b47dcbb05e
beto <franck_ral@yahoo.com>
2017-12-03 22:34:37 -0400

    An excersise was completed. Added pages for Customers and Movies

--- AspNetMvc.ProjectExample/MovieShop/Controllers/MoviesController.cs
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/MoviesController.cs
@@ -47,25 +47,43 @@ namespace MovieShop.Controllers
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageIndex, string sortBy)
+        public ViewResult Index()
         {
-            if (!pageIndex.HasValue)
-            {
-                pageIndex = 1;
-            }
+            var movies = GetMovies();
 
-            if (string.IsNullOrWhiteSpace(sortBy))
-            {
-                sortBy = "Name";
-            }
-
-            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            return View(movies);
         }
+
+        //There was an issue here, becuase the ambiguos paths.
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //    {
+        //        pageIndex = 1;
+        //    }
+
+        //    if (string.IsNullOrWhiteSpace(sortBy))
+        //    {
+        //        sortBy = "Name";
+        //    }
+
+        //    return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + " / " + month);
+        }
+
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
     }
 }
