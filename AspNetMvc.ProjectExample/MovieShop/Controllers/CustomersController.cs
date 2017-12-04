7d639106af83f043867f6374a2ee28ee2d4c4a99
beto <franck_ral@yahoo.com>
2017-12-03 23:38:28 -0400

    Working with data - Querying Objects

--- AspNetMvc.ProjectExample/MovieShop/Controllers/CustomersController.cs
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/CustomersController.cs
@@ -9,17 +9,34 @@ namespace MovieShop.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            /**
+             * When we have the following statement: var customers = _context.Customers;
+             * It does not query to database yet. Until customers variable is iterated.
+             * So If we get ToList as below, It will already query the database.
+             */
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -27,16 +44,6 @@ namespace MovieShop.Controllers
             }
 
             return View(customer);
-        }
-
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
