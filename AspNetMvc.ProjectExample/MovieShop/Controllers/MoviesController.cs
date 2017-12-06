90fe1a8e5a30f76beaead82b4ed14f1ee2c0d9c6
beto <franck_ral@yahoo.com>
2017-12-06 01:37:15 -0400

    Working with data - Display list of movies and details - Added Genre model and updated Movie model

--- AspNetMvc.ProjectExample/MovieShop/Controllers/MoviesController.cs
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/MoviesController.cs
@@ -10,6 +10,19 @@ namespace MovieShop.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
@@ -49,7 +62,7 @@ namespace MovieShop.Controllers
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.ToList();
 
             return View(movies);
         }
@@ -76,14 +89,5 @@ namespace MovieShop.Controllers
             return Content(year + " / " + month);
         }
 
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
     }
 }
