8eb6a0c77821a94b5adddadf6cf75cfd1007a198
Franco Arratia <franco.arratia@lime-energy.com>
2017-12-18 20:13:13 -0400

    UnitTests-Assertions-Learning about code coverage with dotCover from Resharper

---
+++ UnitTests.ProjectExample/FirstUTProject/TestDouble.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace FirstUTProject
+{
+    public static class Solve
+    {
+        public static Tuple<double, double> Quadratic(double a, double b, double c)
+        {
+            var disc = b * b - 4 * a * c;
+            if (disc < 0)
+            {
+                throw new Exception("Cannot solve with complex roots");
+            }
+            else
+            {
+                var root = Math.Sqrt(disc);
+                return Tuple.Create(
+                    (-b + root) / 2 / a,
+                    (-b - root) / 2 / a
+                );
+            }
+        }
+    }
+
+    [TestFixture]
+    class EquationTests
+    {
+        [Test]
+        public void Test()
+        {
+            var result = Solve.Quadratic(1, 10, 16);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            //var result = Solve.Quadratic(1, 1, 1);
+
+            Assert.Throws<Exception>(() =>
+            {
+                Solve.Quadratic(1, 1, 1);
+            });
+        }
+    }
+}
