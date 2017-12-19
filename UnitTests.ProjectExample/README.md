9a82eb382746e54be407b2e432bc6638fcee90c4
Franco Arratia <franco.arratia@lime-energy.com>
2017-12-18 22:37:33 -0400

    UnitTests-TestDoubles-Learning about Static fakes.

--- UnitTests.ProjectExample/README.md
+++ UnitTests.ProjectExample/README.md
@@ -6,3 +6,20 @@
 	- For external go to: https://github.com/NUnitSoftware/nunit-gui/releases and download nunit-gui, you can open the test dll. This is going to be loaded by reflection.
 	- With Visual Studio plug-in go to: https://github.com/nunit/nunit3-vs-adapter/releases and add to visual studio.
 	- With Visual Studio find test explorer. You will have a UI for this purpose.
+	
+# Test Doubles:
+1. We need test double for each object
+2. Fake
+	a) An object that conforms to the predefined interface.
+	b) Does nothing at all, returns default values.
+	c) Null object pattern
+3. Stub
+	a) Like a fake, but returns the answers you define.
+	b) Can record some info about calls that were made.
+4. Mock
+	a) A fake object where you can set expectations.
+	b) Values returned, number of calls made, what parameters were used for the calls.
+5. Moles/Shims
+	a) Moles is a 'detouring' framework by Microsoft.
+	b) Uses profiling feature of CLR to redirect method calls to custom delegates.
+	c) E.g., you can mock DateTime.Now
---
+++ UnitTests.ProjectExample/TestDoublesProject/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDoublesProject
+{
+    public interface ILog
+    {
+        void Write(string msg);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+        private readonly ILog log;
+
+
+        public BankAccount(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void Deposit(int amount)
+        {
+            log.Write($"Depositing {amount}");
+            Balance += amount;
+        }
+    }
+
+
+    public class NullLog : ILog
+    {
+        public void Write(string msg)
+        {
+            
+        }
+    }
+
+    [TestFixture]
+    public class BankAccountTests
+    {
+        private BankAccount ba;
+
+        /**
+         * Because using ConsoleLog, this is an integration test.
+         */
+        [Test]
+        public void DepositIntegrationTest()
+        {
+            ba = new BankAccount(new ConsoleLog()) { Balance = 100 };
+            ba.Deposit(100);
+
+            Assert.That(ba.Balance, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void DepositUnitTestWithFake()
+        {
+            var log = new NullLog();
+            ba = new BankAccount(log) { Balance = 100 };
+            ba.Deposit(100);
+            
+            Assert.That(ba.Balance, Is.EqualTo(200));
+        }
+    }
+}
