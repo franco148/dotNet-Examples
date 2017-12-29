5305d704a0d3cc9215d7379577cfb02853cad39f
Franco Fral <franck_ral@yahoo.com>
2017-12-29 18:22:41 -0400

    ASP.MVC 5-Implementing Validations-Working on Refactoring Magic Numbers.

--- AspNetMvc.ProjectExample/MovieShop/Models/MembershipType.cs
+++ AspNetMvc.ProjectExample/MovieShop/Models/MembershipType.cs
@@ -14,5 +14,11 @@ namespace MovieShop.Models
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte PayAsYouGo = 1;
+        //public static readonly byte Unknown = 0;
+        //public static readonly byte Unknown = 0;
     }
 }
