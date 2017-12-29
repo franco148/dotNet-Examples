5e3f445b081db58849cefd6cb1e726984fa7ba12
Franco Fral <franck_ral@yahoo.com>
2017-12-29 18:12:05 -0400

    ASP.MVC 5-Implementing Validations-Learning about Custom Validations.

--- AspNetMvc.ProjectExample/MovieShop/Models/Customer.cs
+++ AspNetMvc.ProjectExample/MovieShop/Models/Customer.cs
@@ -18,6 +18,7 @@ namespace MovieShop.Models
         public bool IsSubscribedToNewsletter { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
 
         public MembershipType MembershipType { get; set; }
