42cab5720125145fa082d949287043b231fa0129
Franco Fral <franck_ral@yahoo.com>
2017-12-29 17:42:02 -0400

    ASP.MVC 5-Implementing Validations-Learning about adding validations.

--- AspNetMvc.ProjectExample/MovieShop/Controllers/CustomersController.cs
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/CustomersController.cs
@@ -68,6 +68,17 @@ namespace MovieShop.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
