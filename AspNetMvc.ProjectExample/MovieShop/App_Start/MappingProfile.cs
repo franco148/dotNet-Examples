bb02abf7f1ed743ba36d147b66f1e6d5f64fc1af
Franco Fral <franck_ral@yahoo.com>
2018-11-19 22:18:37 -0400

    aspNet-webApi: Added required dependencies and configured AutoMapper in the project for converting a DTO to a domain model and viceversa.

---
+++ AspNetMvc.ProjectExample/MovieShop/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using MovieShop.Dtos;
+using MovieShop.Models;
+
+namespace MovieShop.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
