6138e981f4d3eb966908ffb3f5ed417b08c99eae
AVANTICA\franz.arratia <franz.arratia@avantica.net>
2018-11-20 09:49:57 -0400

    AspNetWebApi-MoviesController: Added Movie DTO and configured in Mapping MappingProfile class for Automapper.

---
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/Api/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using MovieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieShop.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+    }
+}
