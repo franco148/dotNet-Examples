98059575254aab7a821ac6e3459ec8153634adb2
AVANTICA\franz.arratia <franck_ral@hotmail.com>
2018-11-22 11:43:59 -0400

    AspMvcWebApi-RentalFeature: Implemented the logic for creating a rental of movies to a customer.

--- AspNetMvc.ProjectExample/MovieShop/Controllers/Api/RentalsController.cs
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/Api/RentalsController.cs
@@ -1,4 +1,5 @@
 ﻿using MovieShop.Dtos;
+using MovieShop.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,36 @@ namespace MovieShop.Controllers.Api
 {
     public class RentalsController : ApiController
     {
+        private ApplicationDbContext _context;
+
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
 
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto rentalDto)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == rentalDto.CustomerId);
+            var movies = _context.Movies.Where(m => rentalDto.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
