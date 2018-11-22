7b00fb6553d5be39a5928bc39c08eedd0429a382
AVANTICA\franz.arratia <franck_ral@hotmail.com>
2018-11-22 11:09:33 -0400

    AspMvcWebApi-RentalFeature: Created new DTO, Model and Controller for Rental feature.

---
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/Api/RentalsController.cs
@@ -0,0 +1,20 @@
+﻿using MovieShop.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovieShop.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDto rentalDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
