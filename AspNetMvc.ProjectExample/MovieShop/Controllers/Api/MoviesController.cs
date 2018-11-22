c01f69ba197735b37c9c4863c511f6647ef079be
AVANTICA\franz.arratia <franck_ral@hotmail.com>
2018-11-22 14:35:42 -0400

    AspMvcWebApi-RentalFeature: Implementing autocompletion feature. Implemented the filtering for movies based on the value that is typed in the text input.

--- AspNetMvc.ProjectExample/MovieShop/Controllers/Api/MoviesController.cs
+++ AspNetMvc.ProjectExample/MovieShop/Controllers/Api/MoviesController.cs
@@ -22,12 +22,14 @@ namespace MovieShop.Controllers.Api
         }
 
         // GET /api/movies
-        public IHttpActionResult GetMovies()
+        public IHttpActionResult GetMovies(string query = null)
         {
-            var movies = _context.Movies
-                                 .Include(mbox => mbox.Genre)
-                                 .ToList()
-                                 .Select(Mapper.Map<Movie, MovieDto>);
+            var moviesQuery = _context.Movies.Include(mbox => mbox.Genre);
+
+            if (!string.IsNullOrWhiteSpace(query))
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+                                 
+            var movies =  moviesQuery.ToList().Select(Mapper.Map<Movie, MovieDto>);
             return Ok(movies);
         }
 
