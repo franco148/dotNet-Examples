0edb6c7e349662d291a61f020c231b89c930f424
Franco Fral <franck_ral@hotmail.com>
2019-09-21 16:41:20 -0400

    EntityFramework-LinqQueries: Linq in Sintax in action

--- EntityFramework.ProjectExample/EntityFrameworkLinqQueries/Program.cs
+++ EntityFramework.ProjectExample/EntityFrameworkLinqQueries/Program.cs
@@ -33,6 +33,51 @@ namespace EntityFrameworkLinqQueries
             {
                 Console.WriteLine(course.Name);
             }
+
+            // More Examples
+            var queryCourses = from c in context.Courses
+                where c.Author.Id == 1
+                orderby c.Level descending, c.Name // orderding
+                // select c;
+                select new {Name = c.Name, Author = c.Author.Name}; // Projection
+
+            // Grouping
+            var queryGroupingCourses = from c in context.Courses
+                group c by c.Level
+                into g
+                select g;
+
+            foreach (var groupingCourse in queryGroupingCourses)
+            {
+                Console.WriteLine(groupingCourse.Key);
+                foreach (var course in groupingCourse)
+                {
+                    Console.WriteLine("\t{0}", course.Name);
+                }
+            }
+
+            // Count
+            foreach (var groupingCourse in queryGroupingCourses)
+            {
+                Console.WriteLine("{0} ({1})", groupingCourse.Key, groupingCourse.Count());
+            }
+
+            // Joining
+            var queryJoining1 = from c in context.Courses
+                join a in context.Authors on c.AuthorId equals a.Id 
+                //select new {CourseName = c.Name, AuthorName = c.Author.Name};
+                select new { CourseName = c.Name, AuthorName = a.Name };
+
+            var queryJoining2 = from a in context.Authors
+                join c in context.Courses on a.Id equals c.AuthorId into g
+                select new {AuthorName = a.Name, Courses = g.Count()};
+
+            foreach (var x in queryJoining2)
+            {
+                Console.WriteLine("{0} ({1})", x.AuthorName, x.Courses);
+            }
+
+            Console.ReadLine();
         }
     }
 }
