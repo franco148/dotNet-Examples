e5a1b021077343aeb14227d2e4bd8849c56e2459
Franco Fral <franck_ral@hotmail.com>
2019-09-21 16:19:20 -0400

    EntityFramework-LinqQueries: Linq in Action examples

---
+++ EntityFramework.ProjectExample/EntityFrameworkLinqQueries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Queries;
+
+namespace EntityFrameworkLinqQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+
+            // LINQ Syntax
+            var query = from c in context.Courses
+                where c.Name.Contains("C#")
+                orderby c.Name
+                select c;
+
+            foreach (var course in query)
+            {
+                Console.WriteLine(course.Name);
+            }
+
+            // Extension Methods
+            var courses = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .OrderBy(c => c.Name);
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Name);
+            }
+        }
+    }
+}
