8e51898f8dcf4b176c43bef137fd789d6bddd957
Franco Arratia <franck_ral@hotmail.com>
2025-03-31 23:31:27 -0400

    DOTNET-COURSE: Learning WebApi2025, custom middlewares

---
+++ WebApiDemo2025/WebApiDemo2025/Middleware/CustomMiddleware.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WebApiDemo2025.Middleware
+{
+    public class CustomMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("My Custom Middleware - Starts");
+            await next(context);
+            await context.Response.WriteAsync("My Custom Middleware - Ends");
+        }
+    }
+
+    // Instead of using app.UserMiddleware, here we have another approach.
+    public static class CustomMiddlewareExtension 
+    {
+        public static IApplicationBuilder UseCustomMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
--- WebApiDemo2025/WebApiDemo2025/Program.cs
+++ WebApiDemo2025/WebApiDemo2025/Program.cs
@@ -1,4 +1,8 @@
+using WebApiDemo2025.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<CustomMiddleware>();
 var app = builder.Build();
 
 // app.MapGet("/", () => "Hello World!");
@@ -11,13 +15,17 @@ var app = builder.Build();
 // Middleware Chain 
 // Middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) => {
-    await context.Response.WriteAsync("Hello test");
+    await context.Response.WriteAsync("From middleware 1");
     await next(context);
 });
 
 // Middleware 2
+// app.UseMiddleware<CustomMiddleware>();
+app.UseCustomMiddleware();
+
+// Middleware 3
 app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello test again");
+    await context.Response.WriteAsync("From middleware 3");
 });
 
 app.Run();
