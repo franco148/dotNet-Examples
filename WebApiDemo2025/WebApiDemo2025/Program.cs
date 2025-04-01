9fb8826954375a9b4917ec898b8ad7825b8f1146
Franco Arratia <franck_ral@hotmail.com>
2025-03-31 21:45:09 -0400

    DOTNET-COURSE: Learning WebApi2025, middleware chain example

--- WebApiDemo2025/WebApiDemo2025/Program.cs
+++ WebApiDemo2025/WebApiDemo2025/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+
+//app.Run(async (HttpContext context) => {
+//    await context.Response.WriteAsync("Hello test");
+//});
+
+
+// Middleware Chain 
+// Middleware 1
+app.Use(async (HttpContext context, RequestDelegate next) => {
+    await context.Response.WriteAsync("Hello test");
+    await next(context);
+});
+
+// Middleware 2
+app.Run(async (HttpContext context) => {
+    await context.Response.WriteAsync("Hello test again");
+});
 
 app.Run();
