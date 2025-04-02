669ac80c8a9c1ad750196b6d478a41310ddf58af
Franco Arratia <franck_ral@hotmail.com>
2025-04-01 21:58:15 -0400

    MoviesAPI: Implementing DTO pattern

--- MoviesApi/MoviesApi/Models/Dtos/CreateCategoryDto.cs
+++ MoviesApi/MoviesApi/Models/Dtos/CreateCategoryDto.cs
@@ -1,6 +1,11 @@
-﻿namespace MoviesApi.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Models.Dtos
 {
     public class CreateCategoryDto
     {
+        [Required(ErrorMessage = "Name is a required field")]
+        [MaxLength(100, ErrorMessage = "Max lenght for Name should be 100")]
+        public string Name { get; set; }
     }
 }
