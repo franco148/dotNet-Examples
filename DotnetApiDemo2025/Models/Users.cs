a7e1388faf590872c0274e9a5d5f3b2bdb9e80a2
Franco Arratia <franck_ral@hotmail.com>
2025-04-08 00:01:09 -0400

    DotnetApiDemo2025: Implementing the requiredm models

--- DotnetApiDemo2025/Models/Users.cs
+++ DotnetApiDemo2025/Models/Users.cs
@@ -1,12 +1,21 @@
 namespace DotnetApiDemo2025.Models
 {
-    public partial class UseCors
+    public partial class Users
     {
-        public int UseHttpsRedirection { get; set; }
+        public int UserId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public int Gender { get; set; }
+        public string Gender { get; set; }
         public bool Active { get; set; }
+
+        public Users() 
+        {
+            FirstName ??= "";
+            LastName ??= "";
+            Email ??= "";
+            Gender ??= "";
+        }
     }
+    
 }
